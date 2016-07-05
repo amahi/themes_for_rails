a59f3a1ffd03745fd19eba0944c117004ca929a7
Keith Lawrence <keith@kludge.co.uk>
2016-07-05 11:03:10 +0100

    properly scope and include backend

--- lib/themes_for_rails/i18n/backend.rb
+++ lib/themes_for_rails/i18n/backend.rb
@@ -1,15 +1,15 @@
 module ThemesForRails
   module I18n
-    class Backend
+    class Backend < ::I18n::Backend::Simple
   		def initialize(theme_name)
         @theme_name = theme_name
-  			@delegate = I18n::Backend::Simple.new
+  			@delegate = ::I18n::Backend::Simple.new
   		end
 
-      def translate(locale, key)
-        theme_key = "#{theme_name}.#{key}"
+      def translate(locale, key, options)
+        theme_key = "#{@theme_name}.#{key}"
         key = theme_key if @delegate.exists?(locale, theme_key)
-        @delegate.translate(locale, key)
+        @delegate.translate(locale, key, options)
       end
     end
   end
